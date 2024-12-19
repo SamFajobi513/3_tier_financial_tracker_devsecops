124f666496e7531710f31ff2c8c0b5d1c079f78e
SamFajobi <olusegunfajobi12@gmail.com>
2024-12-19 22:04:30 +0100

    Schema

--- Application_Code/backend/src/schema/financial-records.ts
+++ Application_Code/backend/src/schema/financial-records.ts
@@ -8,3 +8,12 @@ interface FinancialRecord {
   category: string;
   paymentMethod: string;
 }
+
+const financialRecordSchema = new mongoose.Schema<FinancialRecord>({
+  userId: { type: String, required: true },
+  date: { type: Date, required: true },
+  description: { type: String, required: true },
+  amount: { type: Number, required: true },
+  category: { type: String, required: true },
+  paymentMethod: { type: String, required: true },
+});
