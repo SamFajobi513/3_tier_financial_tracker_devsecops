ac55e9b515397991d406393ebf80aa4395c1a685
SamFajobi <olusegunfajobi12@gmail.com>
2024-12-19 21:27:09 +0100

    initiate server

---
+++ Application_Code/backend/index.ts
@@ -0,0 +1,10 @@
+import express, { Express } from "express";
+import mongoose from "mongoose";
+import financialRecordRouter from "./routes/financial-records";
+import cors from "cors";
+
+const app: Express = express();
+const port = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(cors());
