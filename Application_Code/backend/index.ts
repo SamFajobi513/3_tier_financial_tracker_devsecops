567a3a36660013ba3f2a1420475090c1d13555fb
SamFajobi <olusegunfajobi12@gmail.com>
2024-12-19 21:29:16 +0100

    configure mongoDb

--- Application_Code/backend/index.ts
+++ Application_Code/backend/index.ts
@@ -8,3 +8,11 @@ const port = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(cors());
+
+const mongoURI: string =
+  "mongodb+srv://machadop1407:UvOUTpZW7EH85MZl@personalfinancetracker.qb0edtk.mongodb.net/";
+
+mongoose
+  .connect(mongoURI)
+  .then(() => console.log("CONNECTED TO MONGODB!"))
+  .catch((err) => console.error("Failed to Connect to MongoDB:", err));
