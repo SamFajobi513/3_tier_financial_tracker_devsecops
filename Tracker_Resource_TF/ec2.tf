525fd6cb57a7ad85ec56cc8a8c5c9adec95dcf06
SamFajobi <olusegunfajobi12@gmail.com>
2025-01-03 22:34:45 +0100

    ec2 terraform update

--- Tracker_Resource_TF/ec2.tf
+++ Tracker_Resource_TF/ec2.tf
@@ -5,5 +5,12 @@ resource "aws_instance" "ec2" {
   subnet_id              = aws_subnet.public-subnet.id
   vpc_security_group_ids = [aws_security_group.security-group.id]
   iam_instance_profile   = aws_iam_instance_profile.instance-profile.name
+  root_block_device {
+    volume_size = 30
+  }
+  user_data = templatefile("./tools-install.sh", {})
 
+  tags = {
+    Name = var.instance-name
+  }
 }
