648040c90ad88d86b6e1efa6f7ff679c95a0a5b5
SamFajobi <olusegunfajobi12@gmail.com>
2025-01-03 22:31:50 +0100

    terraform

--- Tracker_Resource_TF/backend.tf
+++ Tracker_Resource_TF/backend.tf
@@ -1,1 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket         = "my-ews-baket1"
+    region         = "us-east-1"
+    key            = "End-to-End-Kubernetes-Three-Tier-DevSecOps-Project/Jenkins-Server-TF/terraform.tfstate"
+    dynamodb_table = "Lock-Files"
+    encrypt        = true
+  }
 
+}
